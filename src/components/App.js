25b7fb6bf2d1e4695297f41ab3da828406d012ff
chrisbrainsick <chrisbrainsick@gmail.com>
2018-01-27 17:17:52 +0000

    React app with a pollsters select list

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PartySelector from './PartySelector';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PartySelector />
+      </div>
+    );
+  }
+}
+
+export default App;

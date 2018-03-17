0c798371b3d5aeb0ec7808de6e827da5d0676395
chrisbrainsick <chrisbrainsick@gmail.com>
2018-03-17 18:11:48 +0000

    Render a graph with chart js

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,4 +1,42 @@
 import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+
+// Don't render "Don't know" and "Other" results
+const ignoredParties = ['DK', 'OTH'];
+
+const mapPollsToGraphData = polls => {
+  const dates = polls.map(poll => new Date(poll.date).toLocaleDateString('en-GB'));
+  const lines = polls.reduce((lines, poll) => {
+    Object.values(poll.headline).forEach(headline => {
+      if (ignoredParties.includes(headline.party.code)) {
+        return;
+      }
+
+      const partyLine = lines.find(line => line.label === headline.party.name);
+
+      if (!partyLine) {
+        const colour = `#${headline.party.colour}`;
+
+        lines.push({
+          label: headline.party.name,
+          data: [headline.pct],
+          borderColor: colour,
+          backgroundColor: colour,
+          fill: false
+        });
+      } else {
+        partyLine.data.push(headline.pct);
+      }
+    });
+
+    return lines;
+  }, []);
+
+  return {
+    labels: dates,
+    datasets: lines
+  };
+};
 
 class Graph extends Component {
   constructor(props) {
@@ -6,7 +44,7 @@ class Graph extends Component {
 
     this.state = {
       pollster: props.pollster,
-      polls: []
+      graphData: {}
     }
   }
 
@@ -20,7 +58,11 @@ class Graph extends Component {
         return response.json();
       })
       .then(polls => {
-        this.setState({ polls, pollster });
+        console.log(polls)
+        this.setState({
+          graphData: mapPollsToGraphData(polls),
+          pollster
+        });
       });
   }
 
@@ -29,13 +71,18 @@ class Graph extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.pollster != this.props.pollster) {
+    if (nextProps.pollster !== this.props.pollster) {
       this.loadPolls(nextProps.pollster);
     }
   }
 
   render() {
-    return JSON.stringify(this.state.polls);
+    return <Line
+      data={this.state.graphData}
+      legend={{
+        position: 'bottom'
+      }}
+    />
   }
 }
 
